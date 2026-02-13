e2a8173c66e91650361f37767d730a09b577ca19
Eberechi Uchechi Grace <uchechieberechi@gmail.com>
2026-02-13 23:07:37 +0100

    Initail commit

---
+++ android/app/src/main/kotlin/com/example/safepath/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.safepath
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'SafePath',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('SafePath'), centerTitle: true),
+      body: const Center(child: Text('SafePath Guardian')),
+    );
+  }
+}
